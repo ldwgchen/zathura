1eedb279b3f5822c2312d32336092c4f5222ce4a
Xuanli <ludwigchen@proton.me>
2025-02-22 21:52:46 +0800

    Reset position after setting adjust mode

--- zathura/shortcuts.c
+++ zathura/shortcuts.c
@@ -130,6 +130,7 @@ bool sc_adjust_window(girara_session_t* session, girara_argument_t* argument, gi
 
     zathura_document_set_adjust_mode(zathura_get_document(zathura), argument->n);
     adjust_view(zathura);
+    position_set(zathura, -1, -1);
   }
 
   return false;
